d255cc709a480f82a2ad5b942b71ab8e816c0474
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2025-06-07 17:01:01 +0530

    Add files via upload

---
+++ decoder2to4.v
@@ -0,0 +1,16 @@
+`timescale 1ns / 1ps
+module decoder2to4(in,out);
+input wire [1:0]in;
+output reg [3:0]out;
+always@(*) begin
+casez(in)
+2'b00:out=4'b0001;
+2'b01:out=4'b0010;
+2'b10:out=4'b0100;
+2'b11:out=4'b1000;
+default:out=4'b0000;
+endcase
+end
+endmodule
+
+
