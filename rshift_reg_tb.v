98bd766ae82d6b102f70276769e03a95e15fdb8a
Aarathi lakshmi devi kundrapu <153606218+aarthild@users.noreply.github.com>
2025-06-25 21:49:28 +0530

    Add files via upload

---
+++ rshift_reg_tb.v
@@ -0,0 +1,20 @@
+`timescale 1ns / 1ps
+module rshift_reg_tb;
+reg rst,clk,in;
+wire [3:0]out;
+rshift_reg u1(.in(in),.clk(clk),.rst(rst),.out(out));
+initial begin
+clk=0;
+forever #5 clk=~clk;
+end
+initial
+begin
+$monitor("in=%b | rst=%0b | clk=%0b | out=%b",in,rst,clk,out);
+rst=0;#10;
+in=0;#10;
+in=1;#10;
+in=0;#10;
+in=1;#10;
+$finish;
+end
+endmodule
